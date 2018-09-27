3dc16d7f0cf28e26a8042e76c1b79e45a07d2ad9
Godfrid <godfrid@gmail.com>
2018-09-27 10:23:22 +0200

    init

---
+++ src/main/java/hu/flowacademy/timetablemanager/TimetablemanagerApplication.java
@@ -0,0 +1,12 @@
+package hu.flowacademy.timetablemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimetablemanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimetablemanagerApplication.class, args);
+	}
+}
