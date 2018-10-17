b721600b81cf1bc4537f92b923f97f562d444335
Godfrid <godfrid@gmail.com>
2018-10-17 15:20:41 +0200

    code clean up

--- src/main/java/hu/flowacademy/timetablemanager/repository/UserRepository.java
+++ src/main/java/hu/flowacademy/timetablemanager/repository/UserRepository.java
@@ -14,8 +14,6 @@ import java.util.List;
 public interface UserRepository extends JpaRepository<User, Long> {
     List<User> findByGroupId(Long id);
 
-    //TODO: find by group name.
-
     @Query("select u from User u " +
             "inner join u.subjects as subjects where :subject_id = subjects.id")
     List<User> findAllBySubjectId(@Param("subject_id") Long subjectId);
